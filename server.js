7de6d1dfaaaf5da9cce9ed72cf5a4cd0696242d6
Josias Duarte Busiquia <iquabius@gmail.com>
2016-08-08 08:54:57 -0400

    Servidores agora conversam (infinitamente).

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 // http://stackoverflow.com/questions/6177423/send-broadcast-datagram
+var ip = require('ip');
+
+var nome = "Node 1";
 var PORT = 6024;
+var HOST = '127.0.0.1';
 var BROADCAST_ADDR = "192.168.2.255";
-// var BROADCAST_ADDR = "192.168.0.255";
 var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
 
-server.bind(function() {
+var init = function() {
   server.setBroadcast(true);
-  setInterval(broadcastNew, 3000);
-});
+  var address = sock.address;
+  console.log(`Cliente UDP escutando em ${address.address}: ${address.port}`);
+  broadcastNew();
+};
 
 function broadcastNew() {
-  var message = new Buffer("Broadcast message!");
+  var message = new Buffer(`${nome}: Broadcast message!`);
   server.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
-    console.log("Sent '" + message + "'");
+      console.log(`Enviado "${message}".`);
   });
 }
 
 server.on('message', function (message, rinfo) {
-  console.log('Message from: ' + rinfo.address + ':' + rinfo.port +' - ' + message);
+  console.log(`Mensagem recebida: ${rinfo.address}: ${rinfo.port} - ${message}`);
+  if (ip.address() != rinfo.address) {
+    var response = new Buffer(`${nome}: Bem vindo!`);
+    server.send(response, 0, response.length, rinfo.port, rinfo.address, function() {
+      console.log(`Novo nó ${rinfo.address}`);
+    });
+  }
 });
+
+server.bind(PORT, init);
