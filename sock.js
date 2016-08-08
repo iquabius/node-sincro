b1801b4c8881a92bbe5c4b82e2531768ecda3c9b
Josias Duarte Busiquia <iquabius@gmail.com>
2016-08-08 18:27:24 -0400

    Renomei 'server' para 'sock'.

--- sock.js
+++ sock.js
@@ -6,10 +6,10 @@ var PORT = 6024;
 var HOST = '127.0.0.1';
 var BROADCAST_ADDR = "192.168.2.255";
 var dgram = require('dgram');
-var server = dgram.createSocket("udp4");
+var sock = dgram.createSocket("udp4");
 
 var init = function() {
-  server.setBroadcast(true);
+  sock.setBroadcast(true);
   var address = sock.address;
   console.log(`Cliente UDP escutando em ${address.address}: ${address.port}`);
   broadcastNew();
@@ -17,19 +17,19 @@ var init = function() {
 
 function broadcastNew() {
   var message = new Buffer(`${nome}: Broadcast message!`);
-  server.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
+  sock.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
       console.log(`Enviado "${message}".`);
   });
 }
 
-server.on('message', function (message, rinfo) {
+sock.on('message', function (message, rinfo) {
   console.log(`Mensagem recebida: ${rinfo.address}: ${rinfo.port} - ${message}`);
   if (ip.address() != rinfo.address) {
     var response = new Buffer(`${nome}: Bem vindo!`);
-    server.send(response, 0, response.length, rinfo.port, rinfo.address, function() {
+    sock.send(response, 0, response.length, rinfo.port, rinfo.address, function() {
       console.log(`Novo nó ${rinfo.address}`);
     });
   }
 });
 
-server.bind(PORT, init);
+sock.bind(PORT, init);
