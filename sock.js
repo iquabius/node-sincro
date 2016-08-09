da1623315aefc67b46ed80395caaf2017bf73329
Josias Duarte Busiquia <iquabius@gmail.com>
2016-08-09 13:26:13 -0400

    Troquei 'var' por 'let' e 'const'.

--- sock.js
+++ sock.js
@@ -1,22 +1,22 @@
 // http://stackoverflow.com/questions/6177423/send-broadcast-datagram
-var ip = require('ip');
+let ip = require('ip');
 
-var nome = "Node 1";
-var PORT = 6024;
-var HOST = '127.0.0.1';
-var BROADCAST_ADDR = "192.168.2.255";
-var dgram = require('dgram');
-var sock = dgram.createSocket("udp4");
+let nome = "Node 1";
+const PORT = 6024;
+const HOST = '127.0.0.1';
+const BROADCAST_ADDR = "192.168.2.255";
+const dgram = require('dgram');
+const sock = dgram.createSocket("udp4");
 
-var init = function() {
+let init = function() {
   sock.setBroadcast(true);
-  var address = sock.address;
+  let address = sock.address;
   console.log(`Cliente UDP escutando em ${address.address}: ${address.port}`);
   broadcastNew();
 };
 
 function broadcastNew() {
-  var message = new Buffer(`${nome}: Broadcast message!`);
+  let message = new Buffer(`${nome}: Broadcast message!`);
   sock.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
       console.log(`Enviado "${message}".`);
   });
@@ -25,7 +25,7 @@ function broadcastNew() {
 sock.on('message', function (message, rinfo) {
   console.log(`Mensagem recebida: ${rinfo.address}: ${rinfo.port} - ${message}`);
   if (ip.address() != rinfo.address) {
-    var response = new Buffer(`${nome}: Bem vindo!`);
+    let response = new Buffer(`${nome}: Bem vindo!`);
     sock.send(response, 0, response.length, rinfo.port, rinfo.address, function() {
       console.log(`Novo nó ${rinfo.address}`);
     });
